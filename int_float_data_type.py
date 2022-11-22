21bae7e824446a54f56b6f9cd2a1e301e36c2337
tejass11 <118799892+tejass11@users.noreply.github.com>
2022-11-22 13:03:04 +0530

    Add files via upload

---
+++ int_float_data_type.py
@@ -0,0 +1,15 @@
+a=25
+print(type(a))
+b=-62
+print(type(b))
+print(type(25))
+
+x=2.0
+print(type(x))
+a=-0.1
+print(type(a))
+
+print(type(2+0.1))
+print(type(5+10))
+print(type(-2.5-1))
+print(type(-4-5))
